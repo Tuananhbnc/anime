f133a0acd947ecfa04effa605c2603ea3c42707a
TuanAnh <anhntd00633@fpt.edu.vn>
2019-12-24 16:50:40 +0700

    xong phan dia va playlist nhac 2

---
+++ app/src/main/java/com/example/anime/Fragment/Fragment_Play_list_music_disc.java
@@ -0,0 +1,36 @@
+package com.example.anime.Fragment;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.anime.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Fragment_Play_list_music_disc extends Fragment {
+    View view;
+    CircleImageView circleImageView;
+    ObjectAnimator objectAnimator;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_play_list_music_disc,container,false);
+        circleImageView = view.findViewById(R.id.imageviewcicle);
+        objectAnimator = ObjectAnimator.ofFloat(circleImageView,"rotation",0f,360f);
+        objectAnimator.setDuration(10000);
+        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        objectAnimator.setRepeatMode(ValueAnimator.RESTART);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        return view;
+    }
+}
