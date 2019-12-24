0620b0c3216c06eeefeb84ed8bff3b6ae0125389
TuanAnh <anhntd00633@fpt.edu.vn>
2019-12-24 15:14:14 +0700

    create playousic 3

--- app/src/main/java/com/example/anime/Activity/PlaymusicActivity.java
+++ app/src/main/java/com/example/anime/Activity/PlaymusicActivity.java
@@ -1,9 +1,16 @@
 package com.example.anime.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.anime.Model.Song;
@@ -11,10 +18,17 @@ import com.example.anime.R;
 
 public class PlaymusicActivity extends AppCompatActivity {
 
+    Toolbar toolbarplaynhac;
+    TextView txtTimesong,txtTotaltimesong;
+    SeekBar sktime;
+    ImageButton imgplay, imgrepeat, imgnext, imgpre, imgrandom;
+    ViewPager viewPagerplaynhac;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playmusic);
+        init();
 
         // floating action
         Intent intent = getIntent();
@@ -23,5 +37,27 @@ public class PlaymusicActivity extends AppCompatActivity {
             Toast.makeText(this, song.getTitle(), Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void init() {
+        toolbarplaynhac = findViewById(R.id.toolbarplaynhac);
+        txtTimesong = findViewById(R.id.textviewtimesong);
+        txtTotaltimesong = findViewById(R.id.textviewtotaltimesong);
+        sktime = findViewById(R.id.seekbarsong);
+        imgplay = findViewById(R.id.imagebuttonplay);
+        imgrepeat = findViewById(R.id.imagebuttonpepeat);
+        imgnext = findViewById(R.id.imagebuttonnext);
+        imgrandom = findViewById(R.id.imagebuttonsuffle);
+        imgpre = findViewById(R.id.imagebuttonpre);
+        viewPagerplaynhac = findViewById(R.id.viewpagerplaynhac);
+        setSupportActionBar(toolbarplaynhac);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarplaynhac.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        toolbarplaynhac.setTitleTextColor(Color.WHITE);
     }
 }
