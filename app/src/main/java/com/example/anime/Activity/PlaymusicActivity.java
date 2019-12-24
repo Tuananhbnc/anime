a9f6d549800e1609cf91628784e96ad8c478d330
TuanAnh <anhntd00633@fpt.edu.vn>
2019-12-24 14:56:59 +0700

    create playousic 2

--- app/src/main/java/com/example/anime/Activity/PlaymusicActivity.java
+++ app/src/main/java/com/example/anime/Activity/PlaymusicActivity.java
@@ -2,8 +2,11 @@ package com.example.anime.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.anime.Model.Song;
 import com.example.anime.R;
 
 public class PlaymusicActivity extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class PlaymusicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playmusic);
+
+        // floating action
+        Intent intent = getIntent();
+        if(intent.hasExtra("song")){
+            Song song = intent.getParcelableExtra("song");
+            Toast.makeText(this, song.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
